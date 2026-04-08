3a1741d92014537596a334011b2eb7da697f5bb4
meoxjdn <120544758+meoxjdn@users.noreply.github.com>
2026-04-08 16:22:20 +0800

    Update wuwa.c

--- src/core/wuwa.c
+++ src/core/wuwa.c
@@ -17,6 +17,9 @@
 #include "wuwa_utils.h"
 #include "hijack_arm64.h"
 
+// [新增] 引入隐藏 TracerPid 的头文件
+#include "wuwa_hide_trace.h" 
+
 static int __init wuwa_init(void) {
     int ret;
     wuwa_info("helo!\n");
@@ -49,7 +52,6 @@ static int __init wuwa_init(void) {
     }
 #endif
 
-
 #if defined(HIDE_SELF_MODULE)
     hide_module();
 #endif
@@ -57,6 +59,11 @@ static int __init wuwa_init(void) {
 #if defined(BUILD_NO_CFI)
     wuwa_info("NO_CFI is enabled, patched: %d\n", cfi_bypass());
 #endif
+
+    // [新增] 启动 TracerPid 隐藏 (允许失败，不阻断主驱动加载)
+    if (wuwa_hide_trace_init() != 0) {
+        wuwa_warn("wuwa_hide_trace_init failed, device might lack Ftrace support.\n");
+    }
 
     return 0;
 
@@ -68,13 +75,16 @@ clean_sig:
     wuwa_proto_cleanup();
 #endif
 
-
 out:
     return ret;
 }
 
 static void __exit wuwa_exit(void) {
     wuwa_info("bye!\n");
+    
+    // [新增] 卸载 TracerPid 隐藏
+    wuwa_hide_trace_exit();
+    
     wuwa_proto_cleanup();
 #if defined(BUILD_HIDE_SIGNAL)
     wuwa_safe_signal_cleanup();
