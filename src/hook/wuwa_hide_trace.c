21d707601e0ba996b148788d1ba05f654556fcd0
meoxjdn <120544758+meoxjdn@users.noreply.github.com>
2026-04-09 09:49:01 +0800

    Update wuwa_hide_trace.c

--- src/hook/wuwa_hide_trace.c
+++ src/hook/wuwa_hide_trace.c
@@ -8,26 +8,22 @@ struct proc_status_data {
     unsigned int orig_ptrace;
 };
 
-/* 入口 Hook：抹除 TracerPid */
 static int entry_proc_pid_status(struct kretprobe_instance *ri, struct pt_regs *regs) {
     struct proc_status_data *data = (struct proc_status_data *)ri->data;
     struct task_struct *task = NULL;
-
 #ifdef CONFIG_ARM64
     task = (struct task_struct *)regs->regs[3];
 #endif
-
     if (task) {
         data->task = task;
         data->orig_ptrace = task->ptrace;
-        task->ptrace = 0; // 强行置 0
+        task->ptrace = 0;
     } else {
         data->task = NULL;
     }
     return 0;
 }
 
-/* 返回 Hook：恢复真实状态 */
 static int ret_proc_pid_status(struct kretprobe_instance *ri, struct pt_regs *regs) {
     struct proc_status_data *data = (struct proc_status_data *)ri->data;
     if (data->task) {
@@ -46,13 +42,8 @@ static struct kretprobe trace_kretprobe = {
 int wuwa_hide_trace_init(void) {
     int err;
     trace_kretprobe.kp.symbol_name = "proc_pid_status";
-    
     err = register_kretprobe(&trace_kretprobe);
-    if (err < 0) {
-        pr_err("wuwa: Failed to register kretprobe for hiding TracerPid: %d\n", err);
-        return err;
-    }
-
+    if (err < 0) return err;
     pr_info("wuwa: TracerPid auto-hiding activated!\n");
     return 0;
 }
