1f9a15e03ab18fc092e191729a1ab9f23d3a401c
meoxjdn <120544758+meoxjdn@users.noreply.github.com>
2026-04-09 14:20:15 +0800

    Update wuwa_perf_hbp.h

--- src/hook/wuwa_perf_hbp.h
+++ src/hook/wuwa_perf_hbp.h
@@ -1,12 +1,22 @@
+/* wuwa_perf_hbp.h */
 #ifndef WUWA_PERF_HBP_H
 #define WUWA_PERF_HBP_H
 
 #include <linux/types.h>
 
-/* 注意：wuwa_hbp_req 的定义已移至 wuwa_ioctl.h，此处仅作前向声明 */
-struct wuwa_hbp_req;
+#pragma pack(push, 8)
+struct wuwa_hbp_req {
+    int      tid;
+    uint64_t base_addr;
+    int      fov_on;
+    int      border_on;
+    int      skip_on;
+    int      damage_on;
+    int      maxhp_on;
+};
+#pragma pack(pop)
 
-int wuwa_install_perf_hbp(struct wuwa_hbp_req *req);
+int  wuwa_install_perf_hbp(struct wuwa_hbp_req *req);
 void wuwa_cleanup_perf_hbp(void);
 
-#endif
+#endif /* WUWA_PERF_HBP_H */
